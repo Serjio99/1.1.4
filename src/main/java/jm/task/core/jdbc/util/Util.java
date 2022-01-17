5181bf77288e15c853901442da283f397ed6e87f
Bolotov Alexander <distalker1@gmail.com>
2022-01-17 13:52:50 +0300

    pom fix

--- src/main/java/jm/task/core/jdbc/util/Util.java
+++ src/main/java/jm/task/core/jdbc/util/Util.java
@@ -1,5 +1,35 @@
 package jm.task.core.jdbc.util;
 
+import org.hibernate.SessionFactory;
+
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Driver;
+
 public class Util {
-    // реализуйте настройку соеденения с БД
+    // соеденение с БД
+    public static Connection connection = null;
+    public static final String URL = "jdbc:mysql://localhost:3306"; // путь к БД верный?
+    public static final String Driver = "com.mysql.cj.jdbc.Driver";
+    public static final String USERNAME = "root";
+    public static final String PASSWORD = "root";
+    public static SessionFactory sessionFactory;
+
+    public static Connection getConnection() {
+        try {
+            Driver driver = new com.mysql.cj.jdbc.Driver();
+            DriverManager.registerDriver(driver);
+            Class.forName(Driver);
+            connection = DriverManager.getConnection(URL, USERNAME, PASSWORD);
+            System.out.println("Подключение успешно!");
+        } catch (ClassNotFoundException e) {
+            e.printStackTrace();
+            System.out.println("JDBS драйвер не нашолся!");
+        } catch (SQLException e) {
+            e.printStackTrace();
+            System.out.println("SQL-ошибка");
+        }
+        return connection;
+    }
 }
